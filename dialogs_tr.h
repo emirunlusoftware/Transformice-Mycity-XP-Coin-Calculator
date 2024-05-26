aceafc62aa2ff25090a47028763a81f35b16b76f
Emir Ünlü <emir_unlu@outlook.com>
2024-05-26 13:08:40 +0300

    Add files via upload

---
+++ dialogs_tr.h
@@ -0,0 +1,21 @@
+﻿#ifndef DIALOGS_TR_H
+#define DIALOGS_TR_H
+using namespace std;
+
+extern string menu[];
+extern string calc_xp[];
+extern string calc_coin[];
+extern string coin_howto[];
+extern string updatelog[];
+extern string about[];
+extern string v101[];
+extern string v102[];
+extern string v1021[];
+extern string v1022[];
+extern string v103[];
+extern string v104[];
+extern string v1041[];
+extern string v1042[];
+extern string extras[];
+
+#endif // DIALOGS_EN.H
